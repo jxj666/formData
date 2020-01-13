ffd7a06e1e0e23054a76956790f7bc93545b1c20
jxj322991 <228322991@qq.com>
2020-01-13 19:16:00 +0800

    fix

--- form-data/vue.config.js
+++ form-data/vue.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Author: jinxiaojian
+ * @Email: jinxiaojian@youxin.com
+ * @Date: 2019-11-25 11:41:46
+ * @LastEditTime : 2020-01-13 19:14:23
+ * @LastEditors  : 靳肖健
+ */
 module.exports = {
-  publicPath: '/formData/form-data/dist/'
+  publicPath: '/form_data/dist/'
 }
